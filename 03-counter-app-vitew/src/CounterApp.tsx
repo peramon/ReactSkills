7c70fc6bd71befb042477240506be329bfb878f3
Paul Ramon <peramon98.18@gmail.com>
2024-07-31 09:14:15 -0500

    Event click

---
+++ 03-counter-app-vitew/src/CounterApp.tsx
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+
+export const CounterApp = ( { value }) => {
+    
+    const handleAdd = () => {
+        console.log('+1');
+        value = 100;
+    }
+
+    return (
+    <>
+        <h1>CounterApp</h1>
+        <h2> { value } </h2>
+        <button type="button" onClick={ handleAdd }>+1</button>
+    </>
+    );
+}
+
+CounterApp.propTypes = {
+    value: PropTypes.number.isRequired
+}
--- 03-counter-app-vitew/src/main.tsx
+++ 03-counter-app-vitew/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App }from './FirstApp'
 import SecondApp from './SecondApp'
+import { CounterApp }from './CounterApp'
 
 import './styles.css'
 
@@ -9,5 +10,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <App />
         <SecondApp />
+        <CounterApp value={123} />
     </React.StrictMode>
 )
